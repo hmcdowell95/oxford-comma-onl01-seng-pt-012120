20077fa907073b8e04b1a021878df0e9556b3ea8
Harley McDowell <hmcdowell95@gmail.com>
2020-01-29 21:51:21 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 2 
+    array.join(" and ")
+  elsif array.size > 2 
+    g = array.pop
+    array.push("and #{g}")
+    array.join(", ")
+  else
+    array.join
+  end
 end
